3cc3e1d3e494a84ec6f3c33b916908165c37468c
cynthiaewu <cynthiawu@snorlax.ucsd.edu>
2020-02-20 17:30:07 -0800

    calculating with sim cpma

--- calculate_empirical_pvalue.py
+++ calculate_empirical_pvalue.py
@@ -1,21 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
 import argparse
 
 def calculate_empirical(sim_file, obs_file, output):
     #sim_zscores = np.loadtxt('/storage/cynthiawu/trans_eQTL/Nerve-Tibial/chr1_gene_snp_eqtls_simzscores100.csv', delimiter='\t')
-    sim_zscores = np.loadtxt(sim_file, delimiter='\t')
-    sim_pvalues = norm.cdf(sim_zscores)
-    num_genes = len(sim_zscores[0])
-    cpma = []
-    # 3623 snps
-    for sim in sim_pvalues:
-        #print(np.log(sim))
-        likelihood = np.mean(np.negative(np.log(sim)))
-        value = -2 * ((((likelihood - 1) * num_genes)/likelihood) - num_genes*np.log(likelihood))
-        cpma.append(value)
-
+    cpma = np.loadtxt(sim_file, delimiter='\t')
     #obs_cpma = pd.read_csv('/storage/cynthiawu/trans_eQTL/Nerve-Tibial/chr1_gene_snp_eqtls_cpma_nofdr.csv', sep='\t')
     obs_cpma = pd.read_csv(obs_file, sep='\t')
     n_iter = len(cpma)
@@ -28,7 +17,7 @@ def calculate_empirical(sim_file, obs_file, output):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--simulated", required=True, help="Input simulated zscores file")
+    parser.add_argument("-s", "--simulated", required=True, help="Input simulated cpma file")
     parser.add_argument("-o", "--observed", required=True, help="Input observed cpma for each snp file")
     parser.add_argument("-e", "--emp_output", required=True, help="Ouptput file with empirical pvalues for each snp")
     params = parser.parse_args()
