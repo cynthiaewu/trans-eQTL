0c1784359f2655bbc3ed71e9fcf3c774f2216a24
cynthiaewu <cynthiawu@snorlax.ucsd.edu>
2020-02-24 11:21:51 -0800

    sorting to index faster

--- calculate_empirical_pvalue.py
+++ calculate_empirical_pvalue.py
@@ -5,12 +5,14 @@ import argparse
 def calculate_empirical(sim_file, obs_file, output):
     #sim_zscores = np.loadtxt('/storage/cynthiawu/trans_eQTL/Nerve-Tibial/chr1_gene_snp_eqtls_simzscores100.csv', delimiter='\t')
     cpma = np.loadtxt(sim_file, delimiter='\t')
+    cpma.sort()
     #obs_cpma = pd.read_csv('/storage/cynthiawu/trans_eQTL/Nerve-Tibial/chr1_gene_snp_eqtls_cpma_nofdr.csv', sep='\t')
     obs_cpma = pd.read_csv(obs_file, sep='\t')
     n_iter = len(cpma)
     pvalue = []
     for obs in obs_cpma['cpma']:
-        pvalue.append((sum(i > obs for i in cpma)+1)/(n_iter+1))
+        index = n_iter - np.searchsorted(cpma, obs)
+        pvalue.append((index + 1)/(n_iter+1))
     obs_cpma.insert(2, "pvalue", pvalue)
     
     obs_cpma.to_csv(output, index=False, header=True, sep='\t')
