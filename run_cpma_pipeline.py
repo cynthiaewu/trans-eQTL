c6b9ce5c1c5b0a6f5d9e3f5b2e1a7ac9a7845b4b
cynthiaewu <cynthiawu@snorlax.ucsd.edu>
2020-06-05 14:24:26 -0700

    finished cpma pipeline

--- run_cpma_pipeline.py
+++ run_cpma_pipeline.py
@@ -11,7 +11,7 @@ def cpma_pipeline(genotype, expression, tissue, covariate, european, output):
     
     #Get the intersecting samples in the expression, genotype, and covariates file
     intersect_cmd = f'python find_sample_intersect.py -g {genotype_file1} -e {expression} -c {covariate} -i {intersect_file} -p {expression_inter} -q {covariates_inter}'.split(' ')
-    #intersect = subprocess.Popen(intersect_cmd).wait()
+    intersect = subprocess.Popen(intersect_cmd).wait()
     print('Intersecting Files Done')
 
     for i in range(1, 23):
@@ -24,7 +24,7 @@ def cpma_pipeline(genotype, expression, tissue, covariate, european, output):
     for i in range(1, 23):
         preprocess_cmd = f'python preprocess_genotypefile.py -g {genotype}/GTExNormalizedSNPGenotypes_chr{i}.table.gz -i {intersect_file} -o {output}/chr{i}/{genotype_inter}_chr{i}'.split(' ')
         #print(preprocess_cmd)
-        #preprocess = subprocess.Popen(preprocess_cmd).wait()
+        preprocess = subprocess.Popen(preprocess_cmd).wait()
         print(f'Finished preprocessing chr{i}')
     print('Preprocessing Genotype Files Done')
 
@@ -33,21 +33,21 @@ def cpma_pipeline(genotype, expression, tissue, covariate, european, output):
     filtered_snp_list = '/storage/cynthiawu/trans_eQTL/GTex_filteredsnps_pos.txt'
     for i in range(1, 23):
         filtersnp_cmd = f'python filter_snps.py -i {output}/chr{i}/{genotype_inter}_chr{i} -s {filtered_snp_list} -o {output}/chr{i}/{genotype_filter}_chr{i} -c {i}'.split(' ')
-        #filtersnp = subprocess.Popen(filtersnp_cmd).wait()
+        filtersnp = subprocess.Popen(filtersnp_cmd).wait()
         print(f'Finished Filtering chr{i}')
     print('Filtering Snps Done')
     
     #Filter genes in expression file
     expression_filter = f'{expression_inter}_filter'
     filtergene_cmd = f'python filter_genes.py -i {expression_inter} -o {expression_filter}'.split(' ')
-    #filtergene = subprocess.Popen(filtergene_cmd).wait()
+    filtergene = subprocess.Popen(filtergene_cmd).wait()
     print('Filtering Genes Done')
   
     #Perform matrix eQTL on all chr to get gene-snp pairs
     eqtl_file = 'gene-snp_eqtls'
     for i in range(1, 23):
         matrix_cmd = f'Rscript gene-SNP_pairs.R {output}/chr{i}/{genotype_filter}_chr{i} {expression_filter} {covariates_inter} {output}/chr{i}/{eqtl_file}_chr{i}'.split(' ')
-        #matrix = subprocess.Popen(matrix_cmd).wait()
+        matrix = subprocess.Popen(matrix_cmd).wait()
         print(f'Finished matrix eQTL for chr{i}')
     print('Finished matrix eQTL for all chr')
 
@@ -72,6 +72,52 @@ def cpma_pipeline(genotype, expression, tissue, covariate, european, output):
         print(f'Finished calculating cpma for chr{i}')
     print('Finished calculating cpma for all chr')
 
+    #Combine zscores of all chr in one file
+    all_out = f'{output}/{eqtl_file}_allchr'
+    all_zscores = f'{all_out}_zscores'
+    cp_cmd = f'cp {output}/chr1/{zscore_file}_chr1 {all_zscores}'.split(' ')
+    cp = subprocess.Popen(cp_cmd).wait()
+    for i in range(2, 23):
+        append_cmd = f'tail -n +2 -q {output}/chr{i}/{zscore_file}_chr{i}  >> {all_zscores}'
+        subprocess.call(append_cmd, shell=True)
+    print('Finished merging all chr zscores to one file')
+
+    #Calculate the gene covariance matrix and mean zscores for genes
+    cov_matrix = f'{all_out}_cov'
+    mzscores = f'{all_out}_meanzscores'
+    cov_cmd = f'python calculate_cov_meanzscores.py -i {all_zscores} -c {cov_matrix} -m {mzscores}'.split(' ')
+    cov = subprocess.Popen(cov_cmd).wait()
+    print('Finished calculating cov matrix and mean zscores')
+
+    #Calculate the eigendecomposition
+    evalues_file = f'{all_out}_evalues'
+    evectors_file = f'{all_out}_Q'
+    eigen_cmd = f'python calculate_edecomposition.py -c {cov_matrix} -e {evalues_file} -q {evectors_file}'.split(' ')
+    eigen = subprocess.Popen(eigen_cmd).wait()
+    print('Finished calculating eigendecomposition')
+
+    #Simulate cpma values from normal distribution with gene covariance matrix and mean zscores
+    num_sim = 5000000
+    sim_file = f'{all_out}_sim{num_sim}_cpma'
+    simulate_cmd = f'python simulate_cpma_chunks.py -z {mzscores} -e {evalues_file} -q {evectors_file} -o {sim_file} -n {num_sim}'.split(' ')
+    simulate = subprocess.Popen(simulate_cmd).wait()
+    print('Finished simulation of cpma values')
+
+    #Compare simulated cpma with observed cpma to get an empirical pvalue for each snp
+    empirical_file = f'{eqtl_file}_empiricalpvalues'
+    for i in range(1, 23):
+        compare_cmd = f'python calculate_empirical_pvalue.py -s {sim_file} -o {output}/chr{i}/{cpma_file}_chr{i} -e {output}/chr{i}/{empirical_file}_chr{i}'.split(' ')
+        compare = subprocess.Popen(compare_cmd).wait()
+    print('Finished calculating empirical pvalues from cpma for all chr')
+
+    #Merge all empirical pvalues of all chr to one file
+    all_epvalues = f'{all_out}_empiricalpvaluess'
+    cp_cmd = f'cp {output}/chr1/{empirical_file}_chr1 {all_epvalues}'.split(' ')
+    cp = subprocess.Popen(cp_cmd).wait()
+    for i in range(2, 23):
+        append_cmd = f'tail -n +2 -q {output}/chr{i}/{empirical_file}_chr{i}  >> {all_epvalues}'
+        subprocess.call(append_cmd, shell=True)
+    print('Finished merging all chr empirical pvalues to one file')
     '''
     finalgenofile = genotype_filter
     finalexpfile = expression_filter
