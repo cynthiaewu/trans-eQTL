23f909bdb9774ee8c92a21add0d90696996bf0b3
Melissa Gymrek <melissagymrek@Melissas-MBP.attlocal.net>
2021-11-17 21:49:30 -0800

    cleaning up old scripts

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # trans-eQTL
-## Methods
-1. Matrix eQTL: Pairwise SNP vs. gene. (genome-wide, or for a subset of SNPs e.g. protein-coding)
-2. CPMA: joint method (SNP vs. all genes, look at p-value distribution)
-3. x-QTL: joint method (SNP vs. all genes, look at mixture of p-values)
 
-#### Instructions of how to run each specific method are in their respective folders.
+** Under construction **
+
+This repository contains utilities for simulating expression datasets and running various methods for trans-eQTL detection.
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+import os
+from setuptools import setup, find_packages
+
+DESCRIPTION = "Toolkit for trans-eQTL analysis"
+LONG_DESCRIPTION = DESCRIPTION
+NAME = "xQTL"
+AUTHOR = "Melissa Gymrek"
+AUTHOR_EMAIL = "mgymrek@ucsd.edu"
+MAINTAINER = "Melissa Gymrek"
+MAINTAINER_EMAIL = "mgymrek@ucsd.edu"
+DOWNLOAD_URL = 'http://github.com/gymreklab/trans-eQTL'
+LICENSE = 'MIT'
+
+# version-keeping code based on pybedtools
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 1
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'xQTL/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      url=DOWNLOAD_URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      python_requires='>=3.6',
+      packages=find_packages(),
+      include_package_data=True,
+      license_file="LICENSE.txt",
+      scripts=["xQTL/testsupport/test_xqtl.sh", "simulate/testsupport/test_simulate.sh"],
+      entry_points={
+          'console_scripts': [
+              'xQTL-simulate=simulate.main:run',
+              'xQTL-run=xQTL.main:run'
+          ],
+      },
+      install_requires=[],
+      classifiers=['Development Status :: 4 - Beta',\
+                       'Programming Language :: Python :: 3.5',\
+                       'License :: OSI Approved :: MIT License',\
+                       'Operating System :: OS Independent',\
+                       'Intended Audience :: Science/Research',\
+                       'Topic :: Scientific/Engineering :: Bio-Informatics']
+     )
