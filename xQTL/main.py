94959d45b6997ddaa9d50109192578de16c58acf
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2021-12-15 09:26:30 -0800

    adding multi-threading

--- xQTL/main.py
+++ xQTL/main.py
@@ -4,7 +4,7 @@
 Tool to simulate expression data
 
 Example command:
-xQTL-run --input test/sim-0/matrixEQTL.out --out test
+xQTL-run --input ../trans-eQTL/test/sim-0/matrixEQTL.out.gz --out test --cpma --xqtl 
 """
 
 import argparse
@@ -16,8 +16,8 @@ import math
 from scipy import stats
 import scipy.optimize
 import sys
-
-#from xQTL import __version__
+import multiprocessing as mp
+from xQTL import __version__
 
 def ERROR(msg):
     sys.stderr.write("[ERROR]: " + msg.strip() + "\n")
@@ -62,96 +62,112 @@ def likelihood_ratio_test(pvals, trueT=None, trueL=None):
     #pvalue = 1 - scipy.stats.chi2.cdf(test_stat, 1)
     return test_stat, bestT, bestL
 
-# calculate pvalues based off of chi square distribution if there is no gene correlation
-def calculate_pvalue_chidist_nocorr():
-    return values
+def RunSNP(snp, tstats, CPMA=False, XQTL=False):
+    results = {}
+    results["snp"] = snp
+    pvalues = 2*stats.norm.cdf(-np.abs(tstats))
+    if CPMA:
+        cpma = calculate_cpma(pvalues)
+        results["CPMA"] = cpma
+        results["CPMA_p"] = None # TODO
+    if XQTL:
+        test_stat, best_T, best_L = calculate_xqtl(pvalues)
+        results["xQTL"] = test_stat
+        results["predicted_T"] = best_T
+        results["predicted_L"] = best_L
+        results["xQTL_p"] = None # TODO
+    return results
 
-# simulate null distribution of xQTL or CPMA values to adjust for gene correlation
-# permute genotypes of existing snps randomly to create null snps and obtain xQTL or CPMA null values
-def simulate_snp_permute():
-    return values
+def WriteSNP(results, outf, CPMA=False, XQTL=False):
+    outitems = [results["snp"]]
+    if CPMA:
+        outitems.extend([results["CPMA"],results["CPMA_p"]])
+    if XQTL:
+        outitems.extend([results["xQTL"],results["predicted_T"], \
+            results["predicted_L"], results["xQTL_p"]])
+    outf.write("\t".join([str(item) for item in outitems])+"\n")
 
-# simulate null distribution of xQTL or CPMA values to adjust for gene correlation
-# perform eigendecomposition to use decomposed elements to simulate xQTL or CPMA null values
-def simulate_eigendecomp():
-    return values
+def worker(job_queue, out_queue):
+    # Keep looking for jobs to process. add results to out_queue
+    while True:
+        item = job_queue.get() # [snp, tstats, args.cpma, args.xqtl]
+        if item == "DONE": break
+        results = RunSNP(*item[0:4])
+        out_queue.put([results, item[2], item[3]])
 
-# calculate empirical pvalues with simulated xQTL or CPMA null distribution
-def calculate_empirical_pvalue():
-    return values
+def writer(out_queue, CPMA, XQTL, out):
+    # Set up output file
+    header = ["SNP"]
+    if CPMA:
+        header.extend(["CPMA","CPMA_p"])
+    if XQTL:
+        header.extend(["xQTL","predicted_T","predicted_L","xQTL_p"])
+    outf = open(f'{out}.tab', "w")
+    outf.write("\t".join(header)+"\n")
+
+    # Keep looking for output jobs from the queue
+    while True:
+        item = out_queue.get() # [results, CPMA, XQTL]
+        if item == "DONE": break
+        WriteSNP(item[0], outf, item[1], item[2])
+
+    outf.close()
 
 def main(args):
     if not os.path.exists(args.input):
         ERROR("Could not find %s"%args.input)
 
-    # Load Matrix eQTL data
-    # TODO - require matrix eQTL results sorted by SNP, then analyze data for one SNP at a time
-    # can parralelize by SNP
-    #data = pd.read_csv(args.input, sep="\t")
-    if args.cpma:
-        cpma_out = open(f'{args.out}_cpma', "w")
-        cpma_out.write(f'SNP\tCPMA\n')
-    if args.xqtl:
-        xqtl_out = open(f'{args.out}_xqtl', "w")
-        xqtl_out.write(f'SNP\txQTL\tpredicted_T\tpredicted_L\n')
+    # Set up job queue and processors
+    # Note right now will use at least two processors
+    # should possibly not do all this if threads==1
+    job_queue = mp.Queue()
+    out_queue = mp.Queue()
+    processes = [mp.Process(target=worker, args=(job_queue, out_queue)) \
+        for i in range(np.max([args.threads-1, 1]))]
+    writer_proc = mp.Process(target=writer, args=(out_queue, args.cpma, args.xqtl, args.out))
+    for p in processes: p.start()
+    writer_proc.start()
 
     MSG("Starting to read input")
+    # TODO add check for SNP sorted
     with gzip.open(args.input,'rt') as f:
-        #header = f.readline().decode("utf-8")
-        header = f.readline()
+        header = f.readline() # SNP gene    beta    t-stat  p-value FDR
+        header_items = header.split()
+        snp_col = header_items.index("SNP")
+        tstat_col = header_items.index("t-stat")
         snp = ''
         tstats = []
         for line in f:
-            #line = line.decode("utf-8")
             values = line.strip().split('\t')
-            cur_snp = values[0]
+            cur_snp = values[snp_col]
             if snp == cur_snp: 
-                tstats.append(float(values[3]))
+                tstats.append(float(values[tstat_col]))
             elif snp:
-                pvalues = 2*stats.norm.cdf(-np.abs(tstats))
-                if args.cpma:
-                    cpma = calculate_cpma(pvalues)
-                    cpma_out.write(f'{snp}\t{cpma}\n')
-                    # write to output
-                if args.xqtl:
-                    test_stat, best_T, best_L = calculate_xqtl(pvalues)
-                    xqtl_out.write(f'{snp}\t{test_stat}\t{best_T}\t{best_L}\n')
+                job_queue.put([snp, tstats, args.cpma, args.xqtl])
             snp = cur_snp
-    if args.cpma:
-        cpma_out.close()
-    if args.xqtl:
-        xqtl_out.close()
-                
-    #zscores = data.pivot_table(index='SNP', columns='gene', values='t-stat')
-
-    # Convert tvals to pvals
-    #print(zscores) # TODO figure this out
-    #pvalues = 2*stats.norm.cdf(-np.abs(zscores))
-    #print(pvalues)
-    #zscores["pval"] = 2*stats.norm.cdf(-np.abs(zscores))
-
-    # TODO - compute test statistics, either 
-    # using CPMA  ./calculate_cpma.py
-    # or xQTL ./calculate_mixturemodel.py
-    # Add options for either
-
-    # TODO - get pval on test stat
-
-    #ERROR("Not implemented")
+        # Make sure final SNP gets run
+        if snp:
+            job_queue.put([snp, tstats, args.cpma, args.xqtl])
+    for i in range(args.threads): job_queue.put("DONE")
+    for p in processes: p.join()
+    out_queue.put("DONE")
+    writer_proc.join()
+    return 0
 
 def getargs(): 
     parser = argparse.ArgumentParser(__doc__)
     run_group = parser.add_argument_group("xQTL run parameters")
     run_group.add_argument("--xqtl", help="Run x-QTL", action="store_true")
     run_group.add_argument("--cpma", help="Run CPMA", action="store_true")
-    run_group.add_argument("--eigendecomp", help="Run eigendecomposition method to adjust for gene correlation", action="store_true")
-    run_group.add_argument("--snpermute", help="Run snp-permute method to adjust for gene correlation", action="store_true")
-    run_group.add_argument("-s", "--seed", help="Seed for random generator", type=int, default=0 )    
+#    run_group.add_argument("--eigendecomp", help="Run eigendecomposition method to adjust for gene correlation", action="store_true")
+#    run_group.add_argument("--snpermute", help="Run snp-permute method to adjust for gene correlation", action="store_true")
+#    run_group.add_argument("-s", "--seed", help="Seed for random generator", type=int, default=0 )    
+    run_group.add_argument("--threads", help="Number of threads to use", type=int, default=1)
     inout_group = parser.add_argument_group("Input/output")
-    inout_group.add_argument("--input", help="Matrix eQTL file", type=str, required=True)
+    inout_group.add_argument("--input", help="Matrix eQTL file. Must be sorted by SNP.", type=str, required=True)
     inout_group.add_argument("--out", help="Output prefix", type=str, required=True)
-    #ver_group = parser.add_argument_group("Version")
-    #ver_group.add_argument("--version", action="version", version = '{version}'.format(version=__version__))
+    ver_group = parser.add_argument_group("Version")
+    ver_group.add_argument("--version", action="version", version = '{version}'.format(version=__version__))
     args = parser.parse_args()
     return args
 
