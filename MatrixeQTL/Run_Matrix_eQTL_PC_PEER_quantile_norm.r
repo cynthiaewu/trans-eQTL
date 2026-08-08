9424305e9381ad098fce68535000170a021382a9
cynthiaewu <cynthiawu.mail@gmail.com>
2026-08-08 11:25:34 -0700

    Adding current matrixeqtl script and removing misc scripts

--- MatrixeQTL/Run_Matrix_eQTL_PC_PEER_quantile_norm.r
+++ MatrixeQTL/Run_Matrix_eQTL_PC_PEER_quantile_norm.r
@@ -6,33 +6,20 @@ parser$add_argument("-g", "--genotype", help="Input genotype file", required=TRU
 parser$add_argument("-e", "--expression", help="Input expression file", required=TRUE)
 parser$add_argument("-o", "--output", help="Output file", required=TRUE)
 parser$add_argument("-c", "--covariates", help="Input covariates file")
+parser$add_argument("-d", "--delimitter", help="Delimitter for files", default="\t")
 args <- parser$parse_args()
-# args <- parser$parse_args(c('--genotype', '--expression', '--output'))
-#accumulate_fn <- get(args$accumulate)
-#print(accumulate_fn(args$integers))
 SNP_file_name <- args$genotype
 expression_file_name <- args$expression
 covariates_file_name <- args$covariates
 output_file_name <- args$output
-#print(c(SNP_file_name, expression_file_name, output_file_name))
-
-#args = commandArgs(trailingOnly = TRUE)
-#SNP_file_name = args[1]
-#expression_file_name = args[2]
-#covariates_file_name = args[3]
-#output_file_name = args[4]
+delim <- args$delimitter
 
 useModel = modelLINEAR;
-#SNP_file_name = "./GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_866Indiv_chr1_vcftools_addedfilter.intersect.table";
-#expression_file_name = "./Nerve-Tibial_Euro_PEER_residual.intersect.tsv";
-#covariates_file_name = character();
-#covariates_file_name = "./gtex_v8_covariates_dropTRIDTH.intersect.tsv";
-#output_file_name = "./matrixeqtltest.out";
 pvOutputThreshold = 1;
 errorCovariance = numeric();
 
 snps = SlicedData$new();
-snps$fileDelimiter = "\t";      # the TAB character
+snps$fileDelimiter = delim;      # the TAB character
 #snps$fileOmitCharacters = "-1"; # denote missing values;
 snps$fileOmitCharacters = "."; # denote missing values;
 snps$fileSkipRows = 1;          # one row of column labels
@@ -41,7 +28,7 @@ snps$fileSliceSize = 2000;      # read file in pieces of 2,000 rows
 snps$LoadFile( SNP_file_name );
 
 gene = SlicedData$new();
-gene$fileDelimiter = "\t";      # the TAB character
+gene$fileDelimiter = delim;      # the TAB character
 gene$fileOmitCharacters = "NA"; # denote missing values;
 gene$fileSkipRows = 1;          # one row of column labels
 gene$fileSkipColumns = 1;       # one column of row labels
@@ -58,7 +45,7 @@ for( sl in 1:length(gene) ) {
 rm(sl, mat);
 
 cvrt = SlicedData$new();
-cvrt$fileDelimiter = "\t";      # the TAB character
+cvrt$fileDelimiter = delim;      # the TAB character
 cvrt$fileOmitCharacters = "NA"; # denote missing values;
 cvrt$fileSkipRows = 1;          # one row of column labels
 cvrt$fileSkipColumns = 1;       # one column of row labels
